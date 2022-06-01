b175bf5059f9318a9401ce82e1e146ad0d31d1ea
Archana Agrawal <archana.agrawal3@outlook.com>
2022-06-01 12:31:04 -0400

    corrected the code; added more functionality

--- todo-list-fe.js
+++ todo-list-fe.js
@@ -1,9 +1,18 @@
+'use strict'
+
 const baseURL = 'http://localhost:3000/api'
 
-const loadList = (id, data) => {
-    document.getElementById(id).innerHTML = JSON.stringify(data, null, '<br>')
+const loadList = (url, id) => {
+    fetch(url).then(
+        (response) => {
+           return response.json()
+        }
+    ).then((data) => {
+        document.getElementById(id).innerHTML = JSON.stringify(data, null, '<br>')
+        }
+    )   
 }
-
+/*
 fetch(`${baseURL}/todos`).then(
     (response) => {
        return response.json()
@@ -12,3 +21,19 @@ fetch(`${baseURL}/todos`).then(
     loadList('maintext', todoData)
 }
 )
+*/
+
+const addTodo = (url, msg) => {
+    fetch(url,
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({message:msg}),
+        }
+    )
+}
+loadList(`${baseURL}/todos`, 'maintext');
+addTodo(`${baseURL}/todo`, 'seventh todo');
+
